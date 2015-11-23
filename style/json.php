9b2d6c3606b59f8b31ee6cfdd68fd36d3ee48628
shahid-omg <shahid39.sk@gmail.com>
2015-11-22 18:36:20 -0800

    version-2

---
+++ style/json.php
@@ -0,0 +1,7 @@
+<?php
+$data = array('name' => 'shahid', 'version' => 'khan','<br>');
+$data1=array('name' => 'anshu', 'version' => 'kumar','<br>');
+
+print (json_encode($data));
+print (json_encode($data1));
+?> 
